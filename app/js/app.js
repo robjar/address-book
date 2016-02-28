ac596fda781f5a7f93096ed7a7345bf887d021d6
Robert <rob_jar@wp.pl>
2016-02-28 17:28:50 +0100

    Clean up

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,6 @@ import angular from 'angular';
 import 'ngstorage';
 //import countriesList from 'country-list';
 import MainCtrl from './controllers/mainCtrl.js';
-import SecondCtrl from './controllers/secondCtrl.js';
 import storageSrv from './services/storageSrv.js';
 import countriesSrv from './services/countriesSrv.js';
 
@@ -11,8 +10,7 @@ import countriesSrv from './services/countriesSrv.js';
 const main = angular.module('main', ['ngStorage'])
   .factory('storageSrv', storageSrv)
   .factory('countriesSrv', countriesSrv)
-  .controller('MainCtrl', MainCtrl)
-  .controller('SecondCtrl', SecondCtrl);
+  .controller('MainCtrl', MainCtrl);
 
 angular.element(document).ready(() => {
   return angular.bootstrap(document, [main.name]);
--- app/js/controllers/secondCtrl.js
+++
@@ -1,5 +0,0 @@
-function SecondCtrl($scope) {
-  $scope.title = 'Let\'s go! Now!';
-}
-  
-export default ['$scope', SecondCtrl];
